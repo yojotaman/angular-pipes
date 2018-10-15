04bd8dde6373616493faa744f7374f69f12b35e8
yojotaman <yojotaman@gmail.com>
2018-10-14 21:51:19 -0500

    Demo uso de pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-pipes';
+  // title = 'angular-pipes';
+  nombre = 'Jonathan';
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  PI = Math.PI;
+  a = 0.234;
+  salario = 1234.5;
+
+  curso = {
+    nombre: 'Analitica de las cosas',
+    profesor: 'Jonathan Salazar',
+    level: 'IoT Gand Master',
+    coordenadas: {
+      long: '12345',
+      lat: '56789'
+    }
+  };
+
+  valorPromesa = new Promise ( ( resolve, reject ) => {
+    setTimeout( () => resolve ('llegó la data'), 3500);
+  });
+
+  fecha = new Date();
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { LOCALE_ID } from '@angular/core';
+
 
 import { AppComponent } from './app.component';
+
+import localeEs from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+registerLocaleData(localeEs);
+
 
 @NgModule({
   declarations: [
@@ -10,7 +17,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'es'}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
